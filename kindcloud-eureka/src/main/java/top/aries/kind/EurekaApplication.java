fc306af5a7b809d9a164b79cedeee44143f450b0
zhangs <kindaries@qq.com>
2018-09-20 14:23:11 +0800

    修改eureka服务注册中心

--- kindcloud-eureka/src/main/java/top/aries/kind/EurekaApplication.java
+++ kindcloud-eureka/src/main/java/top/aries/kind/EurekaApplication.java
@@ -1,5 +1,7 @@
 package top.aries.kind;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
@@ -9,7 +11,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @EnableEurekaServer
 public class EurekaApplication {
 
+    //定义一个全局的记录器，通过LoggerFactory获取
+    private final static Logger logger = LoggerFactory.getLogger(EurekaApplication.class);
+
     public static void main(String[] args) {
+        logger.info("EurekaApplication启动中......");
         SpringApplication.run(EurekaApplication.class, args);
     }
 
