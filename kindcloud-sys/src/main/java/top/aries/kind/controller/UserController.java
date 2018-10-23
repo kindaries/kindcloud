9fca1e0b6dd98665baa4169eb18afd0368316e90
zhangs <kindaries@qq.com>
2018-10-23 09:13:09 +0800

    完善logback日志记录

--- kindcloud-sys/src/main/java/top/aries/kind/controller/UserController.java
+++ kindcloud-sys/src/main/java/top/aries/kind/controller/UserController.java
@@ -1,5 +1,7 @@
 package top.aries.kind.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +19,10 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    private final Logger kind = LoggerFactory.getLogger("kind");
 
     @RequestMapping(value = "/add", produces = {"application/json;charset=UTF-8"})
     public int addUser(User user) {
@@ -37,6 +43,8 @@ public class UserController {
 
     @RequestMapping(value = "/", produces = {"application/json;charset=UTF-8"})
     public String hello() {
+        logger.info("进入了UserController的hello方法！--controller记录");
+        kind.info("进入了UserController的hello方法！--kind记录");
         return "hello,UserController!";
     }
 }
